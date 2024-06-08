49d6a93a09702196b9829df3e204ae90b798a590
Lucas Lima <lucas.c.lima70@gmail.com>
2024-06-08 11:29:47 -0400

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -1,11 +1,13 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   getVisitCount();
 });
-const functionApi = "http://localhost:7071/api/GetResumeCounter";
+const functionApiURL =
+  "https://getresumecounter7.azurewebsites.net/api/GetResumeCounter?code=lkLov0Ett9OVOb3e3ddVPZQZuRI5ewOgD12OWrStnOd-AzFuhjc-Ag%3D%3D";
+const localfunctionApi = "http://localhost:7071/api/GetResumeCounter";
 
 const getVisitCount = () => {
   let count = 30;
-  fetch(functionApi)
+  fetch(functionApiURL)
     .then((response) => {
       return response.json();
     })
